03ff3b347a86142eec3ff0ea10159451da9138d0
JoaquinBTH <jolk19@student.bth.se>
2022-10-31 14:44:45 +0100

    First task completed

--- fork.c
+++ fork.c
@@ -1,18 +1,53 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <sys/wait.h>
+/*
+Sys wait is not required for the program to work, it simply helps in removing any terminal 
+issues that can occur when the parent process finishes before the child processes are done.
+*/
+
 int main(int argc, char **argv)
 {
-    pid_t pid;
+    pid_t pid, pidTwo; // Create a pid_t value for each child process.
     unsigned i;
     unsigned niterations = 100;
-    pid = fork();
-    if (pid == 0) {
+    pid = fork(); //First child process
+    if (pid == -1)
+    {
+        printf("Error creating first child\n");
+    }
+    else if (pid == 0)
+    {
         for (i = 0; i < niterations; ++i)
             printf("A = %d, ", i);
-     } else {
-        for (i = 0; i < niterations; ++i)
-            printf("B = %d, ", i);
     }
+    else //Parent process
+    {
+        pidTwo = fork(); //Second child process
+        if (pidTwo == -1)
+        {
+            printf("Error creating second child\n");
+        }
+        else if (pidTwo == 0)
+        {
+            for (i = 0; i < niterations; ++i)
+                printf("C = %d, ", i);
+        }
+        else //Parent process
+        {
+            for (i = 0; i < niterations; ++i)
+                printf("B = %d, ", i);
+        }
+    }
+
+    if (pid > 0 && pidTwo > 0)
+    {
+        // Parent process
+        pid = wait(NULL); //Waits for the first child to finish its process (not required)
+        pidTwo = wait(NULL); //Waits for the second child to finish its process (not required)
+        printf("\nID of Child 1: %d\nID of Child 2: %d", pid, pidTwo);
+    }
+
     printf("\n");
 }
