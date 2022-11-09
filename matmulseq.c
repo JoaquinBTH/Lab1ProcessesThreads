8edc2eecc288733556a50bd338e38177a959ec45
JoaquinBTH <jolk19@student.bth.se>
2022-11-09 17:56:01 +0100

    Fifteenth task completed

--- matmulseq.c
+++ matmulseq.c
@@ -12,43 +12,63 @@
 #define SIZE 1024
 
 // Define struct for typecasting
-struct threadRow
+struct threadID
 {
-    unsigned int row;
+    unsigned int id;
 };
 
 static double a[SIZE][SIZE];
 static double b[SIZE][SIZE];
 static double c[SIZE][SIZE];
 
-void *init_matrix(void *row)
+void *init_matrix(void *idArg)
 {
-    struct threadRow *i = (struct threadRow *)row;
+    struct threadID *idVal = (struct threadID *)idArg;
 
-    int j;
+    int i, j;
 
-    for (j = 0; j < SIZE; j++)
+    int maxVal = (idVal->id + 1) * 341;
+
+    if(maxVal == 1023)
     {
-        /* Simple initialization, which enables us to easy check
-         * the correct answer. Each element in c will have the same
-         * value as SIZE after the matmul operation.
-         */
-        a[i->row][j] = 1.0;
-        b[i->row][j] = 1.0;
+        maxVal = 1024;
+    }
+
+    for (i = idVal->id * 341; i < maxVal; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            /* Simple initialization, which enables us to easy check
+             * the correct answer. Each element in c will have the same
+             * value as SIZE after the matmul operation.
+             */
+            a[i][j] = 1.0;
+            b[i][j] = 1.0;
+        }
     }
 }
 
-void *matmul_seq(void *row)
+void *matmul_seq(void *idArg)
 {
-    struct threadRow *i = (struct threadRow *)row;
+    struct threadID *idVal = (struct threadID *)idArg;
 
-    int j, k;
+    int i, j, k;
 
-    for (j = 0; j < SIZE; j++)
+    int maxVal = (idVal->id + 1) * 341;
+
+    if(maxVal == 1023)
     {
-        c[i->row][j] = 0.0;
-        for (k = 0; k < SIZE; k++)
-            c[i->row][j] = c[i->row][j] + a[i->row][k] * b[k][j];
+        maxVal = 1024;
+    }
+
+    for (i = idVal->id * 341; i < maxVal; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            c[i][j] = 0.0;
+            for (k = 0; k < SIZE; k++)
+                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+        }
     }
 }
 
@@ -67,35 +87,35 @@ print_matrix(void)
 
 int main(int argc, char **argv)
 {
-    // Initialize all the 1024 threads.
-    pthread_t thread[SIZE];
-    struct threadRow rows[SIZE];
+    // Initialize 3 threads.
+    pthread_t thread[3];
+    struct threadID ids[3];
 
-    //Initialize matrices
-    for (int i = 0; i < SIZE; i++)
+    // Initialize matrices
+    for (int i = 0; i < 3; i++)
     {
-        rows[i].row = i;
-        pthread_create(&thread[i], NULL, init_matrix, (void *)&rows[i]);
+        ids[i].id = i;
+        pthread_create(&thread[i], NULL, init_matrix, (void *)&ids[i]);
     }
 
-    //Join all threads
-    for (int i = 0; i < SIZE; i++)
-    {
-        pthread_join(thread[i], NULL);
-    }
-
-    //Multiply matrices
-    for (int i = 0; i < SIZE; i++)
-    {
-        pthread_create(&thread[i], NULL, matmul_seq, (void *)&rows[i]);
-    }
-
-    //Join all threads
-    for (int i = 0; i < SIZE; i++)
+    // Join all threads
+    for (int i = 0; i < 3; i++)
     {
         pthread_join(thread[i], NULL);
     }
-    // print_matrix();
+
+    // Multiply matrices
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_create(&thread[i], NULL, matmul_seq, (void *)&ids[i]);
+    }
+
+    // Join all threads
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_join(thread[i], NULL);
+    }
+    //print_matrix();
 
     return 0;
 }
